e6bf755d3fe2bb741af93be0a955f9118953f056
LuKezLima <lucas.dbz.lima@Hotmail.com>
2021-07-07 11:13:38 -0300

    Calculo do resultado

--- PROJETO.c
+++ PROJETO.c
@@ -13,6 +13,15 @@ void question2(void);
 void question3(void);
 void question4(void);
 void question5(void);
+float calculate();
+void resultado(void);
+
+int aud[5];
+int cin[5];
+int vis[5];
+int dig[5];
+
+
 
 
 int main(){
@@ -24,6 +33,9 @@ int main(){
 	login();
 	layout();
 	createFile(name,prontuario);
+	resultado();
+	
+		
 
 	return 0;
 }
@@ -191,28 +203,46 @@ void question1(void){
 	printf("\nc) O que me parece melhor. ");
 	printf("\nd) Um estudo preciso e minucioso do assunto. \n");
 	
+	printf("\n");
+	
 	printf("[a] -> ");
-	scanf("%d", &a1);	
+	scanf("%d", &a1);
+	cin[0] = a1;	
 	while(a1<1 || a1>4){
-		scanf("%d", a1);	
+		printf("Digite um número válido [1-4]:\n");
+		printf("[a] -> ");
+		scanf("%d", &a1);
+		cin[0] = a1;	
 	}
 	
 	printf("[b] -> ");
-	scanf("%d", &b1);	
+	scanf("%d", &b1);
+	aud[0] = b1;	
 	while(b1<1 || b1>4 || b1==a1){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[b] -> ");
 		scanf("%d", &b1);
+		aud[0] = b1;
 	}	
 	
 	printf("[c] -> ");
 	scanf("%d", &c1);
+	vis[0] = c1;
 	while(c1<1 || c1>4 || c1==a1 || c1==b1){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[c] -> ");
 		scanf("%d", &c1);
+		vis[0] = c1;
 	}	
 	
 	printf("[d] -> ");
 	scanf("%d", &d1);
+	dig[0] = d1;
 	while(d1<1 || d1>4 || d1==a1 || d1==b1 || d1==c1){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[d] -> ");
 		scanf("%d", &d1);
+		dig[0] = d1;
 	}
 				
 }
@@ -227,28 +257,46 @@ void question2(void){
 	printf("\nc) A lógica do argumento da outra pessoa.");
 	printf("\nd) Se eu entro em contato ou não com os sentimentos reais do outro.\n");
 	
+	printf("\n");
+	
 	printf("[a] -> ");
-	scanf("%d", &a2);	
+	scanf("%d", &a2);
+	aud[1] = a2;	
 	while(a2<1 || a2>4){
-		scanf("%d", a2);	
+		printf("Digite um número válido [1-4]\n");
+		printf("[a] -> ");
+		scanf("%d", &a2);
+		aud[1] = a2;	
 	}
 	
 	printf("[b] -> ");
-	scanf("%d", &b2);	
+	scanf("%d", &b2);
+	vis[1] = b2;	
 	while(b2<1 || b2>4 || b2==a2){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[b] -> ");
 		scanf("%d", &b2);
+		vis[1] = b2;
 	}
 	
 	printf("[c] -> ");
 	scanf("%d", &c2);
+	dig[1] = c2;
 	while(c2<1 || c2>4 || c2==a2 || c2==b2){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[c] -> ");
 		scanf("%d", &c2);
+		dig[1] = c2;
 	}	
 	
 	printf("[d] -> ");
 	scanf("%d", &d2);
+	cin[1] = d2;
 	while(d2<1 || d2>4 || d2==a2 || d2==b2 || d2==c2){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[d] -> ");
 		scanf("%d", &d2);
+		cin[1] = d2;
 	}			
 }
 
@@ -262,28 +310,46 @@ void question3(void){
 	printf("\nc) Pelas palavras que escolho.");
 	printf("\nd) Pelo tom da minha voz.\n");
 	
+	printf("\n");
+	
 	printf("[a] -> ");
-	scanf("%d", &a3);	
+	scanf("%d", &a3);
+	vis[2] = a3;	
 	while(a3<1 || a3>4){
-		scanf("%d", a3);	
+		printf("Digite um número válido [1-4]\n");
+		printf("[a] -> ");
+		scanf("%d", &a3);
+		vis[2] = a3;	
 	}
 	
 	printf("[b] -> ");
-	scanf("%d", &b3);	
+	scanf("%d", &b3);
+	cin[2] = b3;	
 	while(b3<1 || b3>4 || b3==a3){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[b] -> ");
 		scanf("%d", &b3);
+		cin[2] = b3;
 	}	
 	
 	printf("[c] -> ");
 	scanf("%d", &c3);
+	dig[2] = c3;
 	while(c3<1 || c3>4 || c3==a3 || c3==b3){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[c] -> ");
 		scanf("%d", &c3);
+		dig[2] = c3;
 	}	
 	
 	printf("[d] -> ");
 	scanf("%d", &d3);
+	aud[2] = d3;
 	while(d3<1 || d3>4 || d3==a3 || d3==b3 || d3==c3){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[d] -> ");
 		scanf("%d", &d3);
+		aud[2] = d3;
 	}			
 }
 
@@ -297,28 +363,46 @@ void question4(void){
 	printf("\nc) Escolher os móveis mais confortáveis.");
 	printf("\nd) Escolher as combinações de cores mais ricas e atraentes.\n");
 	
+	printf("\n");
+	
 	printf("[a] -> ");
-	scanf("%d", &a4);	
+	scanf("%d", &a4);
+	aud[3] = a4;	
 	while(a4<1 || a4>4){
-		scanf("%d", a4);	
+		printf("Digite um número válido [1-4]\n");
+		printf("[a] -> ");
+		scanf("%d", &a4);
+		aud[3] = a4;	
 	}
 	
 	printf("[b] -> ");
-	scanf("%d", &b4);	
+	scanf("%d", &b4);
+	dig[3] = b4;	
 	while(b4<1 || b4>4 || b4==a4){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[b] -> ");
 		scanf("%d", &b4);
+		dig[3] = b4;	
 	}
 	
 	printf("[c] -> ");
 	scanf("%d", &c4);
+	cin[3] = c4;
 	while(c4<1 || c4>4 || c4==a4 || c4==b4){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[c] -> ");
 		scanf("%d", &c4);
+		cin[3] = c4;
 	}
 	
 	printf("[d] -> ");
 	scanf("%d", &d4);
+	vis[3] = d4;
 	while(d4<1 || d4>4 || d4==a4 || d4==b4 || d4==c4){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[d] -> ");
 		scanf("%d", &d4);
+		vis[3] = d4;
 	}	
 }
 
@@ -332,29 +416,83 @@ void question5(void){
 	printf("\nc) Eu sou muito sensível à maneira como a roupa veste meu corpo.");
 	printf("\nd) Eu respondo fortemente às cores e à aparência de uma sala.\n");
 	
+	printf("\n");
+	
 	printf("[a] -> ");
-	scanf("%d", &a5);	
+	scanf("%d", &a5);
+	aud[4] = a5;	
 	while(a5<1 || a5>4){
-		scanf("%d", a5);	
+		printf("Digite um número válido [1-4]\n");
+		printf("[a] -> ");
+		scanf("%d", &a5);
+		aud[4] = a5;	
 	}
 	
 	printf("[b] -> ");
-	scanf("%d", &b5);	
+	scanf("%d", &b5);
+	dig[4] = b5;	
 	while(b5<1 || b5>4 || b5==a5){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[b] -> ");
 		scanf("%d", &b5);
+		dig[4] = b5;
 	}	
 	
 	printf("[c] -> ");
 	scanf("%d", &c5);
+	cin[4] = c5;
 	while(c5<1 || c5>4 || c5==a5 || c5==b5){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[c] -> ");
 		scanf("%d", &c5);
+		cin[4] = c5;
 	}
 	
 	printf("[d] -> ");
 	scanf("%d", &d5);
+	vis[4] = d5;
 	while(d5<1 || d5>4 || d5==a5 || d5==b5 || d5==c5){
+		printf("Digite um número válido [1-4] ou ainda não utilizado:\n");
+		printf("[d] -> ");
 		scanf("%d", &d5);
+		vis[4] = d5;
 	}			
+}
+
+
+float calculate(int type[5]){
+	float result;
+	int sum = 0;
+	int i;
+	
+	for(i = 0; i<5 ; i++){
+		sum = sum + type[i];
+	}
+	
+	result = sum * 2;
+	
+	
+	return result;
+}
+
+
+void resultado(void){
+	system("cls");
+	printf("\n");
+	printf("============================RESULTADO============================");
+	printf("\n");
+	printf("Visual: %.0f%%", calculate(vis));
+	printf(" || ");
+	printf("Digital: %.0f%% ", calculate(dig));
+	printf(" || ");
+	printf("Cinestésico: %.0f%%", calculate(cin));
+	printf(" || ");
+	printf("Auditivo: %.0f%%", calculate(aud));
+
+
+	
+
+	return 0;
 }
 
 
